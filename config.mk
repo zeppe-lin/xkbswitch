9987ca2dc1171755241b009b114fb4fe2dbe2d4c
Alexandr Savca (chinarulezzz) <alexandr.savca89@gmail.com>
2022-11-16 13:41:17 +0200

    config.mk: build on dragonflybsd too

--- config.mk
+++ config.mk
@@ -5,16 +5,23 @@ VERSION = 0.1.1
 
 # paths
 PREFIX = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/local/include
+X11LIB = /usr/local/lib
+
+# includes and libs
+INCS = -I${X11INC}
+LIBS = -L${X11LIB} -lX11
 
 # flags
-CPPFLAGS = -I$(X11INC) -D_DEFAULT_SOURCE
-CFLAGS = -std=c99 -pedantic -Wall -Wextra -Os
-LDFLAGS = -L$(X11LIB)
-LDLIBS = -lX11
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_S_SOURCE=200809L \
+	   -DVERSION=\"${VERSION}\" ${INCS}
+CFLAGS = -std=c99 -pedantic -Wall -Wextra
+LDFLAGS = ${LIBS}
 
 # compiler and linker
 CC = cc
+
+# vim:cc=72:tw=70
+# End of file.
