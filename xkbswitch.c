2137c63d6d9298df9ad0a51cfad89797962dfe18
Alexandr Savca <alexandr.savca89@gmail.com>
2022-12-09 03:26:19 +0200

    xkbswitch.c: restyle, remove comments

--- xkbswitch.c
+++ xkbswitch.c
@@ -1,5 +1,4 @@
 // See LICENSE file for copyright and license details.
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -9,65 +8,57 @@
 
 int main(int argc, char **argv)
 {
-  int xkbEventType, xkbError, xkbReason;
-  int mjr = XkbMajorVersion, mnr = XkbMinorVersion;
-  Display *display = NULL;
-  XkbStateRec state;
-  Bool status = True;
+	int xkbEventType, xkbError, xkbReason;
+	int mjr = XkbMajorVersion, mnr = XkbMinorVersion;
+	Display *display = NULL;
+	XkbStateRec state;
+	Bool status = True;
 
-  display = XkbOpenDisplay(NULL, &xkbEventType, &xkbError, &mjr, &mnr,
-                          &xkbReason);
-  if (NULL == display)
-  {
-    warnx("Cannot open X display %s", XDisplayName(NULL));
+	display = XkbOpenDisplay(NULL, &xkbEventType, &xkbError, &mjr,
+	                         &mnr, &xkbReason);
+	if (NULL == display) {
+		warnx("Cannot open X display %s", XDisplayName(NULL));
 
-    switch (xkbReason)
-    {
-    case XkbOD_BadServerVersion:
-      warnx("Bad X11 server version");
-      break;
-    case XkbOD_BadLibraryVersion:
-      warnx("Bad XKB library version");
-      break;
-    case XkbOD_ConnectionRefused:
-      warnx("Connection to X server refused");
-      break;
-    case XkbOD_NonXkbServer:
-      warnx("XKB extension is not present");
-      break;
-    default:
-      warnx("Unknown error from XkbOpenDisplay: %d", xkbReason);
-      break;
-    }
-    exit(1);
-  }
+		switch (xkbReason) {
+		case XkbOD_BadServerVersion:
+			warnx("Bad X11 server version");
+			break;
+		case XkbOD_BadLibraryVersion:
+			warnx("Bad XKB library version");
+			break;
+		case XkbOD_ConnectionRefused:
+			warnx("Connection to X server refused");
+			break;
+		case XkbOD_NonXkbServer:
+			warnx("XKB extension is not present");
+			break;
+		default:
+			warnx("Unknown error from XkbOpenDisplay: %d",
+			      xkbReason);
+			break;
+		}
+		exit(1);
+	}
 
-  if (argc == 1)
-  {
-    // Get layout group index in [0..3].
-    status = XkbGetState(display, XkbUseCoreKbd, &state);
-    printf("%d\n", state.group);
-  }
-  else if (argc == 2)
-  {
-    // Set layout or print usage msg if arg is not in [0..3].
-    if (strlen(argv[1]) > 1 || argv[1][0] < 48 || argv[1][0] > 51)
-      goto usage;
+	if (argc == 1) {
+		// Get layout group index in [0..3].
+		status = XkbGetState(display, XkbUseCoreKbd, &state);
+		printf("%d\n", state.group);
+	} else if (argc == 2) {
+		// Set layout or print usage msg if arg is not in [0..3].
+		if (strlen(argv[1]) > 1
+		    || argv[1][0] < 48
+		    || argv[1][0] > 51)
+			goto usage;
+		status = XkbLockGroup(display, XkbUseCoreKbd,
+		                      atoi(argv[1]));
+	} else {
+usage:
+		printf("Usage: %s [0..3]\n"
+		       "Set/Get current layout group index in range "
+		       "[0..3].\n", argv[0]);
+	}
 
-    status = XkbLockGroup(display, XkbUseCoreKbd, atoi(argv[1]));
-  }
-  else
-  {
- usage:
-    printf("Usage: %s [0..3]\n"
-           "Set/Get current layout group index in range [0..3].\n",
-           argv[0]);
-  }
-
-  XCloseDisplay(display);
-
-  return status;
+	XCloseDisplay(display);
+	return status;
 }
-
-/* vim:sw=2:ts=2:sts=2:et:cc=72:tw=70
- * End of file. */
