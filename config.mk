931c39cd9c7726d3736ce3281d4a2a4a45af88f1
Alexandr Savca <alexandr.savca89@gmail.com>
2023-03-04 17:10:40 +0200

    config.mk: add support to build on Net/Open BSD

--- config.mk
+++ config.mk
@@ -5,8 +5,17 @@ VERSION = 0.1.4
 PREFIX    = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/local/include
-X11LIB = /usr/local/lib
+# DragonFlyBSD, FreeBSD
+#X11INC = /usr/local/include
+#X11LIB = /usr/local/lib
+
+# NetBSD, OpenBSD
+#X11INC = /usr/X11R6/include
+#X11LIB = /usr/X11R6/lib
+
+# Linux
+X11INC = /usr/include
+X11LIB = /usr/lib
 
 # includes and libs
 INCS = -I${X11INC}
