40efaefb88d78300ba7ad5245a9b924f83a18a4f
Alexandr Savca <alexandr.savca89@gmail.com>
2023-08-04 04:10:53 +0300

    config.mk: indent fix

--- config.mk
+++ config.mk
@@ -1,34 +1,34 @@
 # project metadata
-NAME = xkbswitch
-VERSION = 0.1.5
-DIST = ${NAME}-${VERSION}
+NAME      = xkbswitch
+VERSION   = 0.1.5
+DIST      = ${NAME}-${VERSION}
 
 # paths
 PREFIX    = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
 # DragonFlyBSD, FreeBSD
-#X11INC = /usr/local/include
-#X11LIB = /usr/local/lib
+#X11INC   = /usr/local/include
+#X11LIB   = /usr/local/lib
 
 # NetBSD, OpenBSD
-#X11INC = /usr/X11R6/include
-#X11LIB = /usr/X11R6/lib
+#X11INC   = /usr/X11R6/include
+#X11LIB   = /usr/X11R6/lib
 
 # Linux
-X11INC = /usr/include
-X11LIB = /usr/lib
+X11INC    = /usr/include
+X11LIB    = /usr/lib
 
 # includes and libs
-INCS = -I${X11INC}
-LIBS = -L${X11LIB} -lX11
+INCS      = -I${X11INC}
+LIBS      = -L${X11LIB} -lX11
 
 # flags
-CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_S_SOURCE=200809L \
-	   -DVERSION=\"${VERSION}\" ${INCS}
-CFLAGS   = -std=c99 -pedantic -Wall -Wextra
-LDFLAGS  = ${LIBS}
+CPPFLAGS  = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_S_SOURCE=200809L \
+	    -DVERSION=\"${VERSION}\" ${INCS}
+CFLAGS    = -std=c99 -pedantic -Wall -Wextra
+LDFLAGS   = ${LIBS}
 
 # compiler and linker
-CC = cc
-LD = ${CC}
+CC        = cc
+LD        = ${CC}
